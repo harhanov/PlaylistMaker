ee2aa9c4e3b323e4a1b7e01e4f854b434fc4f2f6
harhanov <116550773+harhanov@users.noreply.github.com>
2023-05-21 14:03:35 +0300

    MediaPlayer v2

--- app/src/main/java/com/practicum/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/PlayerActivity.kt
@@ -3,7 +3,6 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.*
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -11,9 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.practicum.playlistmaker.utils.DateUtils.formatTrackTime
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.IOException
+
 
 class PlayerActivity : AppCompatActivity() {
 
@@ -22,7 +19,7 @@ class PlayerActivity : AppCompatActivity() {
         private const val STATE_PREPARED = 1
         private const val STATE_PLAYING = 2
         private const val STATE_PAUSED = 3
-        private const val CURRENT_POSITION_REFRESH = 300L
+        private const val CURRENT_POSITION_REFRESH = 200L
     }
 
     private lateinit var trackNameTextView: TextView
@@ -34,13 +31,17 @@ class PlayerActivity : AppCompatActivity() {
     private lateinit var primaryGenreNameTextView: TextView
     private lateinit var countryTextView: TextView
 
-    private lateinit var play: Button
-    private lateinit var currentPosition: TextView
-    private lateinit var playRunnable: Runnable
+    private lateinit var playPauseButton: Button
+    private lateinit var currentPositionView: TextView
+
     private lateinit var timerRunnable: Runnable
     private lateinit var handler: Handler
     private var playerState = STATE_DEFAULT
     private var mediaPlayer = MediaPlayer()
+    private var isPlaying = false
+    private val playIcon = R.drawable.ic_play_arrow
+    private val pauseIcon = R.drawable.pause_button
+    private var currentPositionMillis: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,12 +54,19 @@ class PlayerActivity : AppCompatActivity() {
             finish()
         }
 
-        play = findViewById(R.id.playback_button)
-        currentPosition = findViewById(R.id.playback_progress)
+        playPauseButton = findViewById(R.id.playPauseButton)
+        playPauseButton.setBackgroundResource(playIcon)
+
+        if (savedInstanceState != null) {
+            isPlaying = savedInstanceState.getBoolean("isPlaying", false)
+            currentPositionMillis = savedInstanceState.getInt("currentPositionMillis", 0)
+        }
+
+        currentPositionView = findViewById(R.id.playback_progress)
         val url = intent.getStringExtra("previewUrl") ?: ""
         preparePlayer(url)
 
-        play.setOnClickListener {
+        playPauseButton.setOnClickListener {
             playbackControl()
         }
 
@@ -89,7 +97,6 @@ class PlayerActivity : AppCompatActivity() {
         primaryGenreNameTextView.text = primaryGenreName
         countryTextView.text = country
 
-        playRunnable = Runnable { playbackControl() }
         handler = Handler(Looper.getMainLooper())
 
         val radiusInPixels = this.resources.getDimensionPixelSize(R.dimen.big_cover_corner_radius)
@@ -100,25 +107,18 @@ class PlayerActivity : AppCompatActivity() {
             .placeholder(R.drawable.crocozebra)
             .transform(RoundedCorners(radiusInPixels))
             .into(artworkImageView)
-
     }
 
     override fun onStop() {
         super.onStop()
         resetTimer()
-        if (mediaPlayer.isPlaying || (playerState == STATE_PAUSED))
-         {
-            mediaPlayer.stop()
-        }
-        mediaPlayer.release()
-    }
-
-    private fun setCurrentPosition() {
-        if (playerState == STATE_PREPARED || playerState == STATE_PLAYING) {
-                currentPosition.text = formatTrackTime(mediaPlayer.currentPosition.toLong())
+        try {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.stop()
+            }
+        } catch (_: IllegalStateException) {
         }
     }
-
 
     private fun getCoverArtwork(artworkUrl100: String): String {
         return artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
@@ -136,54 +136,39 @@ class PlayerActivity : AppCompatActivity() {
     }
 
     private fun preparePlayer(url: String) {
-        try {
-            mediaPlayer.setDataSource(url)
-            mediaPlayer.prepareAsync()
-            mediaPlayer.setOnErrorListener { mp, what, extra ->
-                // Обработка ошибки
-                Log.e("PlayerActivity", "Ошибка при подготовке плеера: $what, $extra")
-                // Дополнительные действия, если необходимо
-
-                // Возвращаем true, чтобы указать, что ошибка была обработана
-                true
-            }
-            mediaPlayer.setOnPreparedListener {
-                play.isEnabled = true
+        mediaPlayer.apply {
+            setDataSource(url)
+            setOnPreparedListener {
                 playerState = STATE_PREPARED
-                Log.d("PlayerActivity", "Плеер подготовлен")
+                playPauseButton.setBackgroundResource(playIcon)
+                currentPositionView.text = formatTrackTime(currentPositionMillis.toLong())
             }
-            mediaPlayer.setOnCompletionListener {
-                play.text = "PLAY"
+            setOnCompletionListener {
                 playerState = STATE_PREPARED
+                playPauseButton.setBackgroundResource(playIcon)
+                currentPositionMillis = 0
+                currentPositionView.text = getString(R.string.zero_time)
             }
-        } catch (e: FileNotFoundException) {
-            Log.e("PlayerActivity", "Файл не найден: $url")
-            e.printStackTrace()
-        } catch (e: IOException) {
-            Log.e("PlayerActivity", "Ошибка ввода-вывода при подготовке плеера")
-            e.printStackTrace()
+            prepareAsync()
         }
     }
 
     private fun startPlayer() {
-        currentPosition.text = getString(R.string.zero_time)
+        playPauseButton.setBackgroundResource(pauseIcon)
+        if (currentPositionMillis == mediaPlayer.duration) {
+            currentPositionMillis = 0
+        }
+        mediaPlayer.seekTo(currentPositionMillis)
         mediaPlayer.start()
-        play.text = "PAUSE"
         playerState = STATE_PLAYING
-        timerUpdate(CURRENT_POSITION_REFRESH)
-    }
-
-    private fun pausePlayer() {
-        mediaPlayer.pause()
-        play.text = "PLAY"
-        playerState = STATE_PAUSED
+        isPlaying = true
+        updateCurrentPosition()
     }
 
     private fun playbackControl() {
         when (playerState) {
             STATE_PLAYING -> {
-                pausePlayer()
-                play.isEnabled = true
+                onPause()
             }
             STATE_PREPARED, STATE_PAUSED -> {
                 startPlayer()
@@ -197,21 +182,69 @@ class PlayerActivity : AppCompatActivity() {
         mediaPlayer.release()
     }
 
+    override fun onPause() {
+        super.onPause()
+        if (!isMediaPlayerReleased() && mediaPlayer.isPlaying) {
+            currentPositionMillis = mediaPlayer.currentPosition
+            pauseMediaPlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!isMediaPlayerReleased() && !mediaPlayer.isPlaying) {
+            mediaPlayer.seekTo(currentPositionMillis)
+            pauseMediaPlayer()
+        }
+    }
+
+    private fun pauseMediaPlayer() {
+        mediaPlayer.pause()
+        playerState = STATE_PAUSED
+        playPauseButton.setBackgroundResource(playIcon)
+        isPlaying = false
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("isPlaying", isPlaying)
+        outState.putInt("currentPositionMillis", currentPositionMillis)
+    }
+
+    private fun isMediaPlayerReleased(): Boolean {
+        return try {
+            mediaPlayer.isPlaying
+            false
+        } catch (e: IllegalStateException) {
+            true
+        }
+    }
+
     private fun resetTimer() {
         if (::timerRunnable.isInitialized) {
             handler.removeCallbacks(timerRunnable)
         }
-        if (::playRunnable.isInitialized) {
-            handler.removeCallbacks(playRunnable)
+    }
+
+    private var isTimerRunning = false
+
+    private fun updateCurrentPosition() {
+        if (playerState != STATE_PLAYING) return
+
+        try {
+            val currentPositionText = formatTrackTime(mediaPlayer.currentPosition.toLong())
+            currentPositionView.text = currentPositionText
+            currentPositionMillis = mediaPlayer.currentPosition
+        } catch (_: IllegalStateException) {
+        }
+
+        if (!isTimerRunning) {
+            isTimerRunning = true
+            handler.postDelayed({
+                isTimerRunning = false
+                updateCurrentPosition()
+            }, CURRENT_POSITION_REFRESH)
         }
     }
 
-
-    private fun timerUpdate(seconds: Long) {
-        if (playerState == STATE_PREPARED || playerState == STATE_PLAYING) {
-            setCurrentPosition()
-            timerRunnable = Runnable { timerUpdate(seconds) }
-            handler.postDelayed(timerRunnable, CURRENT_POSITION_REFRESH)
-        }
-    }
 }
